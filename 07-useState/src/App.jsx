0ed0a88f8ab255fd75b3304c56f9c3f0488195ca
Devashish1Dey <devashishdey01@gmail.com>
2026-03-24 18:25:51 +0530

    Understanding usestate

---
+++ 07-useState/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MyComponent from './MyComponent'
+import Counter from './Counter'
+
+const App = () => {
+  return (
+    <div>
+      {/* <MyComponent /> */}
+      <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ 07-useState/src/MyComponent.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function MyComponent (){
+    // const [name, setName] = useState()
+    const [name, setName] = useState("Guest")
+    const [age, setAge] = useState(0)
+    const [isEmployed, setIsEmployed] = useState(false)
+
+    const updateName = ()=>{
+        setName("Roshan")
+    }
+
+    const incrementAge = ()=>{
+        setAge(age+1)
+    }
+
+    const toggleEmpStud=()=>{
+        setIsEmployed(!isEmployed)
+    }
+    return(
+        <div>
+            <p>Name: {name}</p>
+            <button onClick={updateName}>set Name</button>
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment age</button>
+            <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={toggleEmpStud}>Toggle status</button>
+        </div>
+    )
+}
+export default MyComponent
