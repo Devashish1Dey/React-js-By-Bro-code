d3552b49786298cc46ca2ada3f37984ef22ba520
Devashish1Dey <devashishdey01@gmail.com>
2026-03-27 12:49:51 +0530

    Understanding more deep on updater function

--- 10-Updater-function/src/MyComponents.jsx
+++ 10-Updater-function/src/MyComponents.jsx
@@ -2,6 +2,20 @@ import React, { useState } from "react";
 
 const MyComponents = () => {
   const [count, setCount] = useState(0);
+  const [car, setCar] = useState({year: 2024, make: "Ford", model:"Mustang"})
+
+  function handleYearChange(event){
+      // setCar({...car, year: event.target.value}) // or we can also do like this
+      setCar(c => ({...c, year: event.target.value}))
+  }
+
+  function handleMakeChange(event){
+    setCar(c => ({...c, make: event.target.value}))
+  }
+
+  function handleModelChange(event){
+    setCar(c => ({...c, model: event.target.value}))
+  }
 
   const increment = () => {
     setCount(count + 1);
@@ -18,10 +32,15 @@ const MyComponents = () => {
 
   return (
     <div>
-      <p>Count: {count}</p>
+      {/* <p>Count: {count}</p>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
-      <button onClick={increment}>Increment</button>
+      <button onClick={increment}>Increment</button> */}
+
+      <p>Your Favourite car is: {car.year} {car.make} {car.model}</p>
+      <input type="number" value={car.year} onChange={handleYearChange} /><br/>
+      <input type="text" value={car.make} onChange={handleMakeChange} /><br/>
+      <input type="text" value={car.model} onChange={handleModelChange} /><br/>
     </div>
   );
 };
