b69c0ef0db54a1527b83bf15f670aa71afadae39
Devashish1Dey <devashishdey01@gmail.com>
2026-04-11 18:50:43 +0530

    understanding some more concepts of useEffect hooks

---
+++ 15-more_on_UseEffect/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  // useEffects(()=>{},[])
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("Event Listener Added")
+    return () => {
+      window.removeEventListener("resize", handleResize)
+      console.log("Event Listener Removed")
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = `Size: ${width} X ${height}`
+  }, [width, height])
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  return (
+    <div>
+      <p>Window Width: {width}px</p>
+      <p>Window Height: {height}px</p>
+    </div>
+  );
+};
+
+export default App;
